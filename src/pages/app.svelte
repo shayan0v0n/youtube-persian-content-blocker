bd949c8b2f4e3664e24043ef0841f71a6e403b81
shayan vosoughi <shayanwqhw@gmail.com>
2024-08-06 18:26:24 +0330

    feat: add style

--- src/pages/app.svelte
+++ src/pages/app.svelte
@@ -1,6 +1,33 @@
-<script>
+<script lang="ts">
   import { blockPosts } from "../utils/index";
-  blockPosts();
+
+  let IsRunning = JSON.parse(localStorage.getItem("IsRunning") ?? "");
+  let interval: number;
+
+  const startBlocking = () => {
+    interval = setInterval(blockPosts, 500);
+  };
+
+  const stopBlocking = () => {
+    clearInterval(interval);
+  };
+
+  const updateStorage = () => {
+    localStorage.setItem("IsRunning", JSON.stringify(IsRunning));
+  };
+
+  const changeRunner = () => {
+    IsRunning = !IsRunning;
+    updateStorage();
+    if (IsRunning) startBlocking();
+    else stopBlocking();
+  };
+
+  if (IsRunning) {
+    startBlocking();
+  }
 </script>
 
-<!-- <button on:click={() => blockContent()}> Trigger </button> -->
+<button on:click={changeRunner} class="running-btn">
+  {IsRunning ? "Stop" : "Running"}
+</button>
